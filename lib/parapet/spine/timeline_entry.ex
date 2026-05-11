f4bd2199ad6451a08ff9b2d5d73c4f61f7b59ecf
szTheory <szTheory@users.noreply.github.com>
2026-05-11 09:02:25 -0400

    feat(01-01): implement TimelineEntry schema

---
+++ lib/parapet/spine/timeline_entry.ex
@@ -0,0 +1,24 @@
+defmodule Parapet.Spine.TimelineEntry do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Parapet.Spine.Incident
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "parapet_timeline_entries" do
+    field :type, :string
+    field :payload, :map
+
+    belongs_to :incident, Incident, type: :binary_id
+
+    timestamps(type: :utc_datetime_usec)
+  end
+
+  @doc false
+  def changeset(timeline_entry, attrs) do
+    timeline_entry
+    |> cast(attrs, [:type, :payload, :incident_id])
+    |> validate_required([:type, :incident_id])
+  end
+end
