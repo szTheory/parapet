b7e3fa59cd996e90989c549d93dafca3b6d244b6
szTheory <szTheory@users.noreply.github.com>
2026-05-11 14:37:23 -0400

    feat(01-01): define AlertProcessor skeleton interface

---
+++ lib/parapet/spine/alert_processor.ex
@@ -0,0 +1,12 @@
+defmodule Parapet.Spine.AlertProcessor do
+  @moduledoc """
+  Processes alert batches received from webhooks.
+  """
+
+  @doc """
+  Processes a batch of alerts from the webhook payload.
+  """
+  def process_batch(_payload) do
+    :ok
+  end
+end
