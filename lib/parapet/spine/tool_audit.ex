9edfd91abfa1d0b6df3e635b39e2dc1734cde78b
szTheory <szTheory@users.noreply.github.com>
2026-05-11 09:03:14 -0400

    feat(01-01): implement ToolAudit schema

---
+++ lib/parapet/spine/tool_audit.ex
@@ -0,0 +1,27 @@
+defmodule Parapet.Spine.ToolAudit do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Parapet.Spine.TimelineEntry
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "parapet_tool_audits" do
+    field :tool_name, :string
+    field :input, :map
+    field :output, :map
+    field :success, :boolean
+    field :duration_ms, :integer
+
+    belongs_to :timeline_entry, TimelineEntry, type: :binary_id
+
+    timestamps(type: :utc_datetime_usec)
+  end
+
+  @doc false
+  def changeset(tool_audit, attrs) do
+    tool_audit
+    |> cast(attrs, [:tool_name, :input, :output, :success, :duration_ms, :timeline_entry_id])
+    |> validate_required([:tool_name, :input, :success])
+  end
+end
