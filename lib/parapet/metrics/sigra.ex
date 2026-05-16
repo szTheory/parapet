fd738c37583b2a5f9c0e933caa7bec29f1cd27ec
szTheory <szTheory@users.noreply.github.com>
2026-05-16 21:49:59 +0200

    feat(2-01): create Parapet.Metrics.Sigra module
    - Added Parapet.Metrics.Sigra module for signup/login telemetry metrics.

---
+++ lib/parapet/metrics/sigra.ex
@@ -0,0 +1,17 @@
+defmodule Parapet.Metrics.Sigra do
+  @moduledoc """
+  Prometheus metric definitions for Sigra.
+  """
+  import Telemetry.Metrics
+
+  def setup do
+    :ok
+  end
+
+  def metrics do
+    [
+      counter("parapet.journey.login.count", event_name: [:parapet, :journey, :login], tags: [:outcome]),
+      counter("parapet.journey.signup.count", event_name: [:parapet, :journey, :signup], tags: [:outcome, :provider])
+    ]
+  end
+end
