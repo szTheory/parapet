4cc02907fd7b28895bb2ba4630364a5838d609c8
szTheory <szTheory@users.noreply.github.com>
2026-05-16 21:09:26 +0200

    feat(1-2): implement Parapet.Probe.NativeScheduler

---
+++ lib/parapet/probe/native_scheduler.ex
@@ -0,0 +1,36 @@
+defmodule Parapet.Probe.NativeScheduler do
+  @moduledoc """
+  A simple GenServer-based scheduler for executing synthetic probes.
+
+  It uses `:timer.send_interval/2` to periodically trigger probe executions.
+  Useful for standalone or non-clustered setups.
+  """
+  use GenServer
+
+  @doc """
+  Starts the NativeScheduler.
+
+  ## Options
+
+  The first argument should be a list of probes and their intervals in milliseconds.
+  For example: `[{MyProbe, 60_000}]`
+  """
+  def start_link(probes) when is_list(probes) do
+    GenServer.start_link(__MODULE__, probes)
+  end
+
+  @impl true
+  def init(probes) do
+    Enum.each(probes, fn {probe_module, interval_ms} ->
+      :timer.send_interval(interval_ms, {:run_probe, probe_module})
+    end)
+
+    {:ok, probes}
+  end
+
+  @impl true
+  def handle_info({:run_probe, probe_module}, state) do
+    apply(probe_module, :execute, [])
+    {:noreply, state}
+  end
+end
---
+++ test/parapet/probe/native_scheduler_test.exs
@@ -0,0 +1,30 @@
+defmodule Parapet.Probe.NativeSchedulerTest do
+  use ExUnit.Case, async: true
+
+  alias Parapet.Probe.NativeScheduler
+
+  defmodule TestProbe do
+    use Parapet.Probe
+
+    @impl true
+    def run do
+      send(:test_process, :probe_executed)
+      :ok
+    end
+  end
+
+  test "starts and schedules probes" do
+    Process.register(self(), :test_process)
+
+    # Schedule TestProbe to run every 10ms
+    probes = [{TestProbe, 10}]
+    
+    {:ok, pid} = start_supervised({NativeScheduler, probes})
+    
+    # Wait for the probe to execute
+    assert_receive :probe_executed, 100
+    
+    # It should run continuously
+    assert_receive :probe_executed, 100
+  end
+end
