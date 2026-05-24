9c193dd9e0f65ada612413ba3d7c2604ab4d6202
szTheory <szTheory@users.noreply.github.com>
2026-05-24 17:36:57 +0200

    test(17-03): extend generator-content regression test (D-11 layer 3)
    - Add file-path assertions for retry_storm.ex, suppression_drift.ex, partial_backlog_drain.ex
    - Add content-assertion blocks for all three new templates:
      - retry_storm: asserts defmodule RetryStorm + warning:
      - suppression_drift: asserts defmodule SuppressionDrift + warning:
      - partial_backlog_drain: asserts defmodule PartialBacklogDrain + capability: :retry_async_item + warning:
    - Extend four existing template blocks to assert warning: (proves 17-02 deepening landed at generated-content layer)
    - mix test test/mix/tasks/parapet.gen.runbooks_test.exs: 1 test, 0 failures

--- test/mix/tasks/parapet.gen.runbooks_test.exs
+++ test/mix/tasks/parapet.gen.runbooks_test.exs
@@ -16,6 +16,9 @@ defmodule Mix.Tasks.Parapet.Gen.RunbooksTest do
       assert Enum.any?(files, &String.contains?(&1, "lib/test/parapet/runbooks/dead_letter.ex"))
       assert Enum.any?(files, &String.contains?(&1, "lib/test/parapet/runbooks/provider_outage.ex"))
       assert Enum.any?(files, &String.contains?(&1, "lib/test/parapet/runbooks/callback_delay.ex"))
+      assert Enum.any?(files, &String.contains?(&1, "lib/test/parapet/runbooks/retry_storm.ex"))
+      assert Enum.any?(files, &String.contains?(&1, "lib/test/parapet/runbooks/suppression_drift.ex"))
+      assert Enum.any?(files, &String.contains?(&1, "lib/test/parapet/runbooks/partial_backlog_drain.ex"))
 
       stalled_executor_source =
         Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/stalled_executor.ex")
@@ -24,6 +27,7 @@ defmodule Mix.Tasks.Parapet.Gen.RunbooksTest do
       assert stalled_executor_source =~ "defmodule Test.Parapet.Runbooks.StalledExecutor do"
       assert stalled_executor_source =~ "use Parapet.Runbook"
       assert stalled_executor_source =~ "capability: :retry_async_item"
+      assert stalled_executor_source =~ "warning:"
 
       dead_letter_source =
         Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/dead_letter.ex")
@@ -31,6 +35,7 @@ defmodule Mix.Tasks.Parapet.Gen.RunbooksTest do
 
       assert dead_letter_source =~ "defmodule Test.Parapet.Runbooks.DeadLetter do"
       assert dead_letter_source =~ "capability: :requeue_dead_letter"
+      assert dead_letter_source =~ "warning:"
 
       provider_outage_source =
         Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/provider_outage.ex")
@@ -38,6 +43,7 @@ defmodule Mix.Tasks.Parapet.Gen.RunbooksTest do
 
       assert provider_outage_source =~ "defmodule Test.Parapet.Runbooks.ProviderOutage do"
       assert provider_outage_source =~ "capability: :request_manual_provider_check"
+      assert provider_outage_source =~ "warning:"
 
       callback_delay_source =
         Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/callback_delay.ex")
@@ -45,6 +51,29 @@ defmodule Mix.Tasks.Parapet.Gen.RunbooksTest do
 
       assert callback_delay_source =~ "defmodule Test.Parapet.Runbooks.CallbackDelay do"
       assert callback_delay_source =~ "preview_only: true"
+      assert callback_delay_source =~ "warning:"
+
+      retry_storm_source =
+        Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/retry_storm.ex")
+        |> Rewrite.Source.get(:content)
+
+      assert retry_storm_source =~ "defmodule Test.Parapet.Runbooks.RetryStorm do"
+      assert retry_storm_source =~ "warning:"
+
+      suppression_drift_source =
+        Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/suppression_drift.ex")
+        |> Rewrite.Source.get(:content)
+
+      assert suppression_drift_source =~ "defmodule Test.Parapet.Runbooks.SuppressionDrift do"
+      assert suppression_drift_source =~ "warning:"
+
+      partial_backlog_drain_source =
+        Rewrite.source!(igniter.rewrite, "lib/test/parapet/runbooks/partial_backlog_drain.ex")
+        |> Rewrite.Source.get(:content)
+
+      assert partial_backlog_drain_source =~ "defmodule Test.Parapet.Runbooks.PartialBacklogDrain do"
+      assert partial_backlog_drain_source =~ "capability: :retry_async_item"
+      assert partial_backlog_drain_source =~ "warning:"
     end
   end
 end
